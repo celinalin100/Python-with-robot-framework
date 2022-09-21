a5a072cb89685f4a41d8b4dfdb4275b736f76061
celinalin100 <65714462+celinalin100@users.noreply.github.com>
2022-09-21 11:32:26 +0800

    Update draft02.py

--- Robot_test/draft02.py
+++ Robot_test/draft02.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import os
+
 
 s=Service('C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
 chrome_web = webdriver.Chrome(service=s)
@@ -17,14 +17,14 @@ def close_popup():
     # Explicitly wait:
     wait = WebDriverWait(chrome_web, 20)
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#pge_1ywRD0t10 > div.om-global-close-button.om-popup-close > span"))).click()
-    #等待Pop up banner -->點選關閉
+    #Wait Pop up banner -->close the banner
     print("Pop up window is closed successfully!")
-    #進入Furbo網站
+    #Get into Furbb website
 
     chrome_web.close()
     
     pass
     
 
-# close_popup()
+
 
